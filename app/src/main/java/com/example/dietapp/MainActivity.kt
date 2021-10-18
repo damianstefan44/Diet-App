cee5f62bacd5c5cf5e8751156711f320b7fd8c60
Rivus44 <rivus44@gmail.com>
2021-10-18 23:06:45 +0200

    Górny i dolny panel nawigacyjny

--- app/src/main/java/com/example/dietapp/MainActivity.kt
+++ app/src/main/java/com/example/dietapp/MainActivity.kt
@@ -1,22 +1,77 @@
 package com.example.dietapp
 
+import CurrentDayFragment
+import FavouriteProductsFragment
+import MyDietsFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.hide()
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayShowTitleEnabled(false);
 
-        //val toolbar:Toolbar = findViewById(R.id.main_toolbar)
+        //supportActionBar?.hide()
+
+        //val toolbar:Toolbar = findViewById(R.id.toolbar)
         //setSupportActionBar(toolbar)
+
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav)
+        val myDiets = MyDietsFragment()
+        val currentDay = CurrentDayFragment()
+        val favourites = FavouriteProductsFragment()
+
+        val curDay : View = bottomNav.findViewById(R.id.bottom_nav_current_day)
+        curDay.performClick()
+
+        selectFragment(currentDay)
+
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.bottom_nav_current_day->selectFragment(currentDay)
+                R.id.bottom_nav_my_diets->selectFragment(myDiets)
+                R.id.bottom_nav_favourite->selectFragment(favourites)
+
+            }
+            true
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // User chose the "Settings" item, show the app settings UI...
+            Toast.makeText(applicationContext,"ustawienia",Toast.LENGTH_SHORT).show()
+            true
+        }
+
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun selectFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentSelector,fragment)
+            commit()
+        }
 
 
 }
---
+++ app/src/main/java/com/example/dietapp/MyDietsFragment.kt
@@ -0,0 +1,5 @@
+import androidx.fragment.app.Fragment
+import com.example.dietapp.R
+
+class MyDietsFragment:Fragment(R.layout.fragment_my_diets) {
+}
