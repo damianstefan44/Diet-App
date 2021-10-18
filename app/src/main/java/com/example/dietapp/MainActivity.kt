79475555aa299860eb49b62c111fd16abb9f9e08
Rivus44 <rivus44@gmail.com>
2021-10-18 18:51:39 +0200

    Logowanie, rejestracja i początek głównego ekranu

---
+++ app/src/main/java/com/example/dietapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.dietapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        //val toolbar:Toolbar = findViewById(R.id.main_toolbar)
+        //setSupportActionBar(toolbar)
+    }
+
+
+}
