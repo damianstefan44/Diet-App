419179bec2aaa94a515f8dce2c4581c89705b2d7
Rivus44 <rivus44@gmail.com>
2021-11-12 03:16:14 +0100

    Dodano fragment bieżącego dnia i produkty do recyclerów

--- app/src/main/java/com/example/dietapp/CurrentDayFragment.kt
+++ app/src/main/java/com/example/dietapp/CurrentDayFragment.kt
@@ -1,5 +1,52 @@
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
-import com.example.dietapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dietapp.*
 
 class CurrentDayFragment:Fragment(R.layout.fragment_current_day) {
+
+    private var nameList = mutableListOf<String>()
+    private var weightList = mutableListOf<String>()
+    private var caloriesList = mutableListOf<Int>()
+    private var eatenList = mutableListOf<Boolean>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(nameList.isEmpty()){
+            postToList()
+        }
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        val breakfast: RecyclerView = requireView().findViewById<View>(R.id.breakfastRecyclerView) as RecyclerView
+        val secondBreakfast: RecyclerView = requireView().findViewById<View>(R.id.secondBreakfastRecyclerView) as RecyclerView
+        val lunch: RecyclerView = requireView().findViewById<View>(R.id.lunchRecyclerView) as RecyclerView
+        val snack: RecyclerView = requireView().findViewById<View>(R.id.snackRecyclerView) as RecyclerView
+        val dinner: RecyclerView = requireView().findViewById<View>(R.id.dinnerRecyclerView) as RecyclerView
+
+        breakfast.layoutManager = LinearLayoutManager(activity?.applicationContext)
+        breakfast.adapter = ProductAdapter(nameList, weightList, caloriesList, eatenList)
+
+
+
+    }
+
+
+    private fun addToList(name: String, weight: String, calories: Int, eaten: Boolean){
+        nameList.add(name)
+        weightList.add(weight)
+        caloriesList.add(calories)
+        eatenList.add(eaten)
+    }
+
+    private fun postToList(){
+        for(i in 1..5){
+            addToList("Produkt $i", "Gramatura $i",354, false)
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/dietapp/ProductAdapter.kt
@@ -0,0 +1,105 @@
+package com.example.dietapp
+
+import android.R.attr.button
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ProductAdapter(private var names: MutableList<String>,private var weights: MutableList<String>, private var calories: MutableList<Int>,private var eaten: MutableList<Boolean>):
+    RecyclerView.Adapter<ProductAdapter.ViewHolder>()
+{
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        val productName: TextView = itemView.findViewById(R.id.product_name)
+        val productWeight: TextView = itemView.findViewById(R.id.product_weight)
+        val productCalories: TextView = itemView.findViewById(R.id.product_calories)
+        val productEaten: Button = itemView.findViewById(R.id.product_eaten)
+        private val productDelete: ImageView = itemView.findViewById(R.id.product_delete)
+
+        init {
+            productDelete.setOnClickListener { v: View ->
+                val position: Int = adapterPosition
+                removeItem(position)
+            }
+            productEaten.setOnClickListener { v: View ->
+                val position: Int = adapterPosition
+                changeButton(position)
+            }
+
+        }
+
+
+
+    }
+
+        // Assigning values to layout objects
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.product_layout,parent,false)
+        return ViewHolder(v)
+
+        // Inflating layout
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.productName.text = names[position]
+        holder.productWeight.text = weights[position]
+        holder.productCalories.text = calories[position].toString() + " kcal"
+        when (eaten[position]) {
+            true -> {
+                holder.productEaten.text = "Zjedzono"
+                var buttonDrawable: Drawable? = holder.productEaten.background
+                buttonDrawable = DrawableCompat.wrap(buttonDrawable!!)
+                DrawableCompat.setTint(buttonDrawable, Color.rgb(180, 255, 180))
+                holder.productEaten.background = buttonDrawable
+
+            }
+            false -> {holder.productEaten.text = "Zjedz"
+                var buttonDrawable: Drawable? = holder.productEaten.background
+                buttonDrawable = DrawableCompat.wrap(buttonDrawable!!)
+                DrawableCompat.setTint(buttonDrawable, Color.rgb(255, 128, 128))
+                holder.productEaten.background = buttonDrawable
+            }
+        }
+
+        // This is what is going to be seen in each recyclerview item
+    }
+
+    override fun getItemCount(): Int {
+        return names.size
+
+        // Size of lists passed to adapter
+    }
+
+    fun removeItem(position: Int) {
+        names.removeAt(position)
+        weights.removeAt(position)
+        calories.removeAt(position)
+
+        notifyDataSetChanged()
+    }
+
+    fun changeButton(position: Int) {
+        eaten[position] = when (eaten[position]) {
+            true -> false
+            false -> true
+        }
+        notifyDataSetChanged()
+    }
+
+
+
+}
